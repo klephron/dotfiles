118610da47830486a087ed934498b2e42fef500b
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-06-12 22:22:31 +0300

    neovim: Refactor keymaps.lua

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -1,14 +1,15 @@
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true }
 
--- SAVE_FILE
-keymap('n', '<c-s>', ':w<CR>', {})
-keymap('i', '<c-s>', '<Esc>:w<CR>a', {})
--- WINDOWS
+-- Save file
+keymap('n', '<c-s>', ':w<CR>', opts)
+keymap('i', '<c-s>', '<Esc>:w<CR>a', opts)
+-- Window jumping
 keymap('n', '<c-j>', '<c-w>j', opts)
 keymap('n', '<c-h>', '<c-w>h', opts)
 keymap('n', '<c-k>', '<c-w>k', opts)
 keymap('n', '<c-l>', '<c-w>l', opts)
--- TERMINAL
+-- Terminal normal mode emulation
 keymap('t', '<C-[>', '<C-\\><C-n>', opts)
 keymap('t', '<Esc>', '<C-\\><C-n>', opts)
+
