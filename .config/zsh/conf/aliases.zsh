9ed76dbe9fd57ca109c1fdff1d0ccff5e2736635
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-07-02 20:20:20 +1000

    zsh: Update aliases.zsh

--- .config/zsh/conf/aliases.zsh
+++ .config/zsh/conf/aliases.zsh
@@ -4,8 +4,8 @@ alias ll="ls --color -lh"
 alias sysupdate="sudo emaint sync -A && sudo emerge -aDNuv @world --exclude=\"\`awk 'END{print RS}\$0=\$0' ORS=\" \" /etc/portage/package.exclude_desktop\`\""
 alias sysupdateall="sudo emaint sync -A && sudo emerge -aDNuv @world"
 
-alias sysupdate_emerge="sudo emerge -aDNuv @world --exclude=\"\`awk 'END{print RS}\$0=\$0' ORS=\" \" /etc/portage/package.exclude_desktop\`\""
-alias sysupdateall_emerge="sudo emerge -aDNuv @world"
+alias sysupdate.emerge="sudo emerge -aDNuv @world --exclude=\"\`awk 'END{print RS}\$0=\$0' ORS=\" \" /etc/portage/package.exclude_desktop\`\""
+alias sysupdateall.emerge="sudo emerge -aDNuv @world"
 
 # Dirstack
 alias d='dirs -v'
