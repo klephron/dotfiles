6fd3feb986bb420b8c66797e6d8c74a31223aa27
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-07-15 16:24:25 +1000

    nvim: Update hi TSComment (link to Comment)

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -2,7 +2,12 @@ local colorscheme = "darkplus"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 vim.cmd("hi VertSplit gui=NONE")
+
 if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
+
+-- additional options for colorscheme
+vim.cmd("hi clear TSComment")
+vim.cmd("hi link TSComment Comment")
