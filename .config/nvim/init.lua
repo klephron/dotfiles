11a9b73fa0a169ce168106048505eb3e8164c347
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-07-17 22:31:57 +1000

    nvim: Add autocommands

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,5 +3,4 @@ require "user.settings"
 require "user.colorscheme"
 require "user.plugins"
 require "user.autocommands"
-
 -- SERVERS: "user.plugins.lsp.lsp_installer"
--- .config/nvim/lua/user/autocommands.lua
+++ .config/nvim/lua/user/autocommands.lua
@@ -1,11 +1,44 @@
---[[ TODO understand how to write lua autocommands
+--[[ TODO: understand how to write lua autocommands
 vim.api.nvim_create_autocmd( {"User", "VimtexEventInitPost"}, {
     command = "call vimtex#compiler#start()"
 })
 --]]
+
 vim.cmd [[
-augroup vimtex
-    autocmd!
-    au User VimtexEventInitPost call vimtex#compiler#start()
-augroup END
+  augroup _vimtex
+      au!
+      au User VimtexEventInitPost call vimtex#compiler#start()
+  augroup END
+
+  augroup _git
+    au!
+    au FileType gitcommit setlocal wrap
+    au FileType gitcommit setlocal spell
+  augroup end
+  augroup _markdown
+    au!
+    au FileType markdown setlocal wrap
+    au FileType markdown setlocal spell
+  augroup end
+
+  augroup _info_pages
+    au!
+    au FileType help,lspinfo nnoremap <silent> <buffer> q :close<CR> 
+  augroup end
+
+  augroup _binary
+    au! 
+    au BufReadPre   *.bin,*.exe,*.out let &bin=1
+    au BufReadPost  *.bin,*.exe,*.out if &bin | %!xxd
+    au BufReadPost  *.bin,*.exe,*.out set ft=xxd | endif
+    au BufWritePre  *.bin,*.exe,*.out if &bin | %!xxd -r
+    au BufWritePre  *.bin,*.exe,*.out endif
+    au BufWritePost *.bin,*.exe,*.out if &bin | %!xxd
+    au BufWritePost *.bin,*.exe,*.out set nomod | endif
+  augroup end
+
+  augroup _nasm
+    au!
+    au BufRead,BufNewFile *.inc,*.asm set filetype=nasm
+  augroup end
 ]]
