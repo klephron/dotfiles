08801bac3fe7c31e3e4062e49230ece2f283990e
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-08-02 11:44:15 +1000

    nvim: Add cpp configuration (dap)

--- .config/nvim/lua/user/plugins/dap/adapters.lua
+++ .config/nvim/lua/user/plugins/dap/adapters.lua
@@ -1,7 +1,15 @@
 local dap = require('dap')
 
+local HOME = vim.fn.expand('$HOME')
+
 dap.adapters.python = {
   type = 'executable';
-  command = vim.fn.expand('~/.venvs/debugpy/bin/python'),
+  command = HOME .. '/.venvs/debugpy/bin/python',
   args = { '-m', 'debugpy.adapter' };
+}
+
+dap.adapters.cppdbg = {
+  id = 'cppdbg',
+  type = 'executable',
+  command = HOME .. '/Data/cpptools/debugAdapters/bin/OpenDebugAD7',
 }
--- .config/nvim/lua/user/plugins/dap/configurations.lua
+++ .config/nvim/lua/user/plugins/dap/configurations.lua
@@ -1,4 +1,5 @@
 local dap = require('dap')
+
 dap.configurations.python = {
   {
     -- The first three options are required by nvim-dap
@@ -7,7 +8,6 @@ dap.configurations.python = {
     name = "Launch file";
 
     -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
-
     program = "${file}"; -- This configuration will launch the current file if used.
     pythonPath = function()
       -- venv in path
@@ -27,3 +27,31 @@ dap.configurations.python = {
     end;
   },
 }
+
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "cppdbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = true,
+  },
+  {
+    name = 'Attach to gdbserver :1234',
+    type = 'cppdbg',
+    request = 'launch',
+    MIMode = 'gdb',
+    miDebuggerServerAddress = 'localhost:1234',
+    miDebuggerPath = '/usr/bin/gdb',
+    cwd = '${workspaceFolder}',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+  },
+}
+
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
