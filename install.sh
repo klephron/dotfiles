df7b19a93cb27a40311b3d5b9aa19f6a3c5064af
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-08-19 11:08:15 +1000

    tmux: Add option to change variables and then manually update config based on them

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ pwd_dir=`pwd`
 script_dir=`dirname "$0"`
 config_dir=.config
 
-for var in .gvimrc .tmux.conf .vim; do
+for var in .gvimrc .vim; do
   ln -sfn $pwd_dir/$script_dir/$var $HOME/$var
 done
 
-for var in alacritty broot gtk-3.0 htop kdeglobals mimeapps.list nvim plasmarc qimgv qt5ct rofi zathura zsh fzfrc awesome; do
+for var in alacritty broot gtk-3.0 htop kdeglobals mimeapps.list nvim plasmarc qimgv qt5ct rofi zathura zsh fzfrc awesome tmux; do
   ln -sfn $pwd_dir/$script_dir/$config_dir/$var $HOME/.config/$var
 done
 
