dc53cd2e23063293340f2549abc7dc12a541fa70
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-08-24 09:59:29 +1000

    nvim: (neorg) Add wrapper to check if `neorg.is_loaded()` and start if not

--- .config/nvim/lua/user/plugins/neorg.lua
+++ .config/nvim/lua/user/plugins/neorg.lua
@@ -1,23 +1,24 @@
 return function()
   local neorg = require("neorg")
   local kmps = require("user.keymaps").neorg
-  local neorg_started = false
 
-  us.set_keynomap("n", kmps.gtd_capture.key, "<cmd>Neorg gtd capture<cr>", kmps:desc("gtd_capture"))
-  us.set_keynomap("n", kmps.gtd_views.key, "<cmd>Neorg gtd views<cr>", kmps:desc("gtd_views"))
-  us.set_keynomap("n", kmps.kanban_toggle.key, "<cmd>Neorg kanban toggle<cr>", kmps:desc("kanban_toggle"))
-  us.set_keynomap("n", kmps.switch_workspace.key, "<cmd>Telescope neorg switch_workspace<cr>",
-    kmps:desc("switch_workspace"))
-  us.set_keynomap("n", kmps.neorg_start.key, function()
-    if not neorg_started then
-      vim.cmd("NeorgStart")
-      vim.notify('Loaded.', vim.log.levels.INFO, { title = 'Neorg' })
-      neorg_started = true
-    else
-      vim.notify('Already Loaded.', vim.log.levels.INFO, { title = 'Neorg' })
+  local function load_wrapper(callback)
+    return function()
+      if not neorg.is_loaded() then
+        neorg.org_file_entered(true)
+        vim.notify('Loaded.', vim.log.levels.INFO, { title = 'Neorg' })
+        vim.cmd("delcommand NeorgStart")
+      end
+      if type(callback) == 'function' then return callback()
+      elseif type(callback) == 'string' then return vim.cmd(callback) end
     end
-  end, kmps:desc("neorg_start"))
-  us.set_keynomap("n", kmps.journal.key, "<cmd>Neorg journal<cr>", kmps:desc("journal"))
+  end
+  us.set_keynomap("n", kmps.gtd_capture.key, load_wrapper("Neorg gtd capture"), kmps:desc("gtd_capture"))
+  us.set_keynomap("n", kmps.gtd_views.key, load_wrapper("Neorg gtd views"), kmps:desc("gtd_views"))
+  us.set_keynomap("n", kmps.kanban_toggle.key, load_wrapper("Neorg kanban toggle"), kmps:desc("kanban_toggle"))
+  us.set_keynomap("n", kmps.switch_workspace.key, load_wrapper("Telescope neorg switch_workspace"),
+    kmps:desc("switch_workspace"))
+  us.set_keynomap("n", kmps.journal.key, load_wrapper("Neorg journal"), kmps:desc("journal"))
 
   neorg.setup {
     load = {
