25f50e384cd6d7ca3f157369f86971907adb1e35
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-08-25 16:08:17 +1000

    nvim: (project) Silent chdir

--- .config/nvim/lua/user/plugins/project.lua
+++ .config/nvim/lua/user/plugins/project.lua
@@ -31,7 +31,7 @@ return function()
 
     -- When set to false, you will get a message when project.nvim changes your
     -- directory.
-    silent_chdir = false,
+    silent_chdir = true,
 
     -- Path where project.nvim will store the project history for use in
     -- telescope
