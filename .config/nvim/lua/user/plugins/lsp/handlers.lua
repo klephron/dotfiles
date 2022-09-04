6180bb9a5e5216de381eeb4adfe21efd52e29a9a
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-09-04 10:53:33 +0300

    nvim: (lsp) Remove provider check for `on_attach`

--- .config/nvim/lua/user/plugins/lsp/handlers.lua
+++ .config/nvim/lua/user/plugins/lsp/handlers.lua
@@ -54,56 +54,45 @@ function M.on_attach(client, bufnr)
     return { buffer = bufnr, desc = kmps:desc(desc) }
   end
 
-  if has_provider(client, "definitionProvider") then
-    us.set_keynomap("n", kmps.definition.key, vim.lsp.buf.definition, with_desc("definition"))
-    us.set_keynomap("n", kmps.g_definition.key, vim.lsp.buf.definition, with_desc("definition"))
+  -- Editing and movement
+  -- If has "definitionProvider"
+  us.set_keynomap("n", kmps.definition.key, vim.lsp.buf.definition, with_desc("definition"))
+  us.set_keynomap("n", kmps.g_definition.key, vim.lsp.buf.definition, with_desc("definition"))
+  -- If has "declarationProvider"
+  us.set_keynomap("n", kmps.declaration.key, vim.lsp.buf.declaration, with_desc("declaration"))
+  us.set_keynomap("n", kmps.g_declaration.key, vim.lsp.buf.declaration, with_desc("declaration"))
+  -- if has "hoverProvider"
+  us.set_keynomap("n", kmps.hover.key, vim.lsp.buf.hover, with_desc("hover"))
+  us.set_keynomap("n", kmps.g_hover.key, vim.lsp.buf.hover, with_desc("hover"))
+  -- if has "implementationProvider"
+  us.set_keynomap("n", kmps.implementation.key, vim.lsp.buf.implementation, with_desc("implementation"))
+  us.set_keynomap("n", kmps.g_implementation.key, vim.lsp.buf.implementation, with_desc("implementation"))
+  -- if has "renameProvider"
+  us.set_keynomap("n", kmps.rename.key, vim.lsp.buf.rename, with_desc("rename"))
+  -- if has "referencesProvider"
+  us.set_keynomap("n", kmps.references.key, vim.lsp.buf.references, with_desc("references"))
+  us.set_keynomap("n", kmps.g_references.key, vim.lsp.buf.references, with_desc("references"))
+  -- if has "codeActionProvider"
+  if lspsaga_ok then
+    us.set_keynomap("n", kmps.code_action.key, "<cmd>Lspsaga code_action<cr>", with_desc("code_action"))
+  else
+    us.set_keynomap("n", kmps.code_action.key, vim.lsp.buf.code_action, with_desc("code_action"))
   end
-  if has_provider(client, "declarationProvider") then
-    us.set_keynomap("n", kmps.declaration.key, vim.lsp.buf.declaration, with_desc("declaration"))
-    us.set_keynomap("n", kmps.g_declaration.key, vim.lsp.buf.declaration, with_desc("declaration"))
-  end
-  if has_provider(client, "hoverProvider") then
-    us.set_keynomap("n", kmps.hover.key, vim.lsp.buf.hover, with_desc("hover"))
-    us.set_keynomap("n", kmps.g_hover.key, vim.lsp.buf.hover, with_desc("hover"))
-  end
-  if has_provider(client, "implementationProvider") then
-    us.set_keynomap("n", kmps.implementation.key, vim.lsp.buf.implementation, with_desc("implementation"))
-    us.set_keynomap("n", kmps.g_implementation.key, vim.lsp.buf.implementation, with_desc("implementation"))
-  end
-  if has_provider(client, "renameProvider") then
-    us.set_keynomap("n", kmps.rename.key, vim.lsp.buf.rename, with_desc("rename"))
-  end
-  if has_provider(client, "referencesProvider") then
-    us.set_keynomap("n", kmps.references.key, vim.lsp.buf.references, with_desc("references"))
-    us.set_keynomap("n", kmps.g_references.key, vim.lsp.buf.references, with_desc("references"))
-  end
-  if has_provider(client, "codeActionProvider") then
-    if lspsaga_ok then
-      us.set_keynomap("n", kmps.code_action.key, "<cmd>Lspsaga code_action<cr>", with_desc("code_action"))
-    else
-      us.set_keynomap("n", kmps.code_action.key, vim.lsp.buf.code_action, with_desc("code_action"))
-    end
-    -- us.set_keynomap("n", kmps.code_action.key, vim.lsp.buf.range_code_action, with_desc("code_action")) deprecated
-  end
-  if has_provider(client, "signatureHelpProvider") then
-    us.set_keynomap("n", kmps.signature_help_n.key, vim.lsp.buf.signature_help, with_desc("signature_help_n"))
-    require("lsp_signature").on_attach()
-  end
-  if has_provider(client, "typeDefinitionProvider") then
-    us.set_keynomap("n", kmps.type_definition.key, vim.lsp.buf.type_definition, with_desc("type_definition"))
-  end
-  if has_provider(client, "codeLensProvider") then
-    us.set_keynomap("n", kmps.codelens.key, vim.lsp.codelens.run, with_desc("codelens"))
-  end
-  if has_provider(client, "documentSymbolProvider") then
-    require("aerial").on_attach(client, bufnr)
-  end
+  -- us.set_keynomap("n", kmps.code_action.key, vim.lsp.buf.range_code_action, with_desc("code_action")) deprecated
+  -- if has "signatureHelpProvider"
+  us.set_keynomap("n", kmps.signature_help_n.key, vim.lsp.buf.signature_help, with_desc("signature_help_n"))
+  require("lsp_signature").on_attach()
+  -- if has "typeDefinitionProvider"
+  us.set_keynomap("n", kmps.type_definition.key, vim.lsp.buf.type_definition, with_desc("type_definition"))
+  -- if has "codeLensProvider"
+  us.set_keynomap("n", kmps.codelens.key, vim.lsp.codelens.run, with_desc("codelens"))
+  -- if has "documentSymbolProvider"
+  require("aerial").on_attach(client, bufnr)
 
   -- Formattings
-  if has_provider(client, "documentFormattingProvider") then
-    us.set_keynomap("n", kmps.format.key, function() vim.lsp.buf.format({ async = true }) end,
-      with_desc(("format")))
-  end
+  -- if has "documentFormattingProvider"
+  us.set_keynomap("n", kmps.format.key, function() vim.lsp.buf.format({ async = true }) end,
+    with_desc(("format")))
 
   -- Diagnostics
   if lspsaga_ok then
@@ -143,7 +132,7 @@ function M.on_attach(client, bufnr)
   -- selectionRangeProvider
 end
 
--- capabilities
+-- Capabilities
 local status_ok, cmp_nvim_lsp = safe_require("cmp_nvim_lsp")
 if not status_ok then
   return
