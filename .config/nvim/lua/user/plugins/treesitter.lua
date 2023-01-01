e983571346517ebb6389722b5b3777199884cabf
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2023-01-01 14:44:38 +0300

    Update treesitter

--- .config/nvim/lua/user/plugins/treesitter.lua
+++ .config/nvim/lua/user/plugins/treesitter.lua
@@ -6,7 +6,28 @@ local M = {
 function M.config()
   local ts_configs = require("nvim-treesitter.configs")
   ts_configs.setup({
-    ensure_installed = { "c", "cpp", "lua", "vim", "bash", "regex", "markdown", "markdown_inline" },
+    ensure_installed = {
+      "c",
+      "cpp",
+      "lua",
+      "vim",
+      "bash",
+      "regex",
+      "markdown",
+      "markdown_inline",
+      "cmake",
+      "python",
+    },
+    highlight = {
+      enable = true,
+    },
+    indent = {
+      enable = true,
+    },
+    context_commentstring = {
+      enable = true,
+      enable_autocmd = false,
+    },
     sync_install = false,
     auto_install = false,
   })
