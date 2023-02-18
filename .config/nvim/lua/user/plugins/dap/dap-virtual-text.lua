9be56b7e30215238ed1f47b30cd40d614ea40c12
Nikita Kulakov <nvkulakov.dev@gmail.com>
2023-02-18 12:23:59 +0300

    nvim: (dap-virtual-text) Add

---
+++ .config/nvim/lua/user/plugins/dap/dap-virtual-text.lua
@@ -0,0 +1,16 @@
+local M = {
+  'theHamsta/nvim-dap-virtual-text',
+  dependencies = {
+    'mfussenegger/nvim-dap',
+  },
+  event = "VeryLazy",
+  config = function()
+    require("nvim-dap-virtual-text").setup({
+
+    })
+    -- Update highlights
+    vim.api.nvim_set_hl(0, "NvimDapVirtualText", { link = "NonText" })
+  end
+}
+
+return M
--- .config/nvim/lua/user/plugins/dap/init.lua
+++ .config/nvim/lua/user/plugins/dap/init.lua
@@ -6,4 +6,5 @@ local M = {
 }
 -- dependencies
 table.insert(M, conf_require("dap.dap"))
+table.insert(M, conf_require("dap.dap-virtual-text"))
 return M
