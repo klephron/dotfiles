23440812a5f265cc45ec8413c4bf57d7a8ecec25
Nikita Kulakov <nvkulakov.dev@gmail.com>
2023-09-02 10:27:26 +0300

    Update vimtex config

--- .config/nvim/lua/user/plugins/vimtex.lua
+++ .config/nvim/lua/user/plugins/vimtex.lua
@@ -4,15 +4,11 @@ local M = {
   config = function()
     vim.g.tex_flavor = 'latex'
     vim.g.vimtex_view_method = 'zathura'
-    -- vim.g.vimtex_indent_on_ampersands = 0
+    vim.g.vimtex_indent_on_ampersands = 0
     vim.g.vimtex_indent_enabled = 0
     vim.g.vimtex_quickfix_mode = 0
     vim.g.vimtex_matchparen_enabled = 0
     vim.g.vimtex_syntax_enabled = 0
-    vim.g.vimtex_compiler_latexmk = {
-
-      build_dir = 'build',
-    }
     -- vim.cmd("hi clear Conceal")
   end
 }
