48acbf607487a065f9b0f8117c84731ea92fe261
Nikita Kulakov <nvkulakov.dev@gmail.com>
2023-11-20 08:01:00 +0300

    update aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -4,8 +4,8 @@ alias ls="ls --color -F"
 alias ll="ls --color -lh"
 alias info='info --vi-keys'
 
-alias sysupdate="sudo eix-sync && sudo emerge -aDNuv @world --exclude=\"\`awk 'END{print RS}\$0=\$0' ORS=\" \" /etc/portage/package.exclude_desktop\`\""
-alias sysupdateall="sudo eix-sync && sudo emerge -aDNuv @world"
+alias sysupdate="sudo emerge --sync && sudo emerge -aDNuv @world --exclude=\"\`awk 'END{print RS}\$0=\$0' ORS=\" \" /etc/portage/package.exclude_desktop\`\""
+alias sysupdateall="sudo emerge --sync && sudo emerge -aDNuv @world"
 
 alias sysupdate.emerge="sudo emerge -aDNuv @world --exclude=\"\`awk 'END{print RS}\$0=\$0' ORS=\" \" /etc/portage/package.exclude_desktop\`\""
 alias sysupdateall.emerge="sudo emerge -aDNuv @world"
