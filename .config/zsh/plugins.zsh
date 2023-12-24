b1dada361fa3d2fcd041c3297de06f58f00a0977
Nikita Kulakov <nvkulakov.dev@gmail.com>
2023-12-24 16:06:57 +0300

    feat: add zsh autosuggestions

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,13 +1,11 @@
 CONFIG_BINDKEYS=$XDG_CONFIG_HOME/zsh/bindkeys.zsh
 
 # PLUGINS
-source /usr/share/zsh/site-functions/zsh-syntax-highlighting.zsh
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-source /usr/share/zsh/site-functions/zsh-autosuggestions.zsh
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#777777"
 ZSH_AUTOSUGGEST_STRATEGY="history"
-
-source /usr/local/share/zsh/site-functions/bd-zsh/bd.plugin.zsh
 
 source /usr/share/fzf/key-bindings.zsh
 if [[ -f ~/.config/fzf ]]; then
