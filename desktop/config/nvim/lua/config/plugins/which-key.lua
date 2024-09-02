7c624241f9362bc1d6515272512293ced7ebd57f
Nikita Kulakov <nvkulakov.dev@gmail.com>
2024-09-02 19:01:43 +0300

    fix warning

--- desktop/config/nvim/lua/config/plugins/which-key.lua
+++ desktop/config/nvim/lua/config/plugins/which-key.lua
@@ -2,7 +2,6 @@ local M = {
   "folke/which-key.nvim",
   opts = {
     show_help = false,
-    triggers = "auto",
     disable = {
       buftypes = config.is_vscode and {"", "acwrite", "help", "nofile", "nowrite", "quickfix", "terminal", "prompt"} or {}
     }
