7208f9adb35676cc654faa421cb9ed9126ab5229
Nikita Kulakov <nvkulakov.dev@gmail.com>
2024-09-03 12:50:13 +0300

    change sh for dev-container

--- scripts/setup-dev-container.sh
+++ scripts/setup-dev-container.sh
@@ -2,5 +2,8 @@
 
 echo "export XDG_CONFIG_HOME=~/.config" >> ~/.profile
 echo 'export ZDOTDIR=$XDG_CONFIG_HOME/zsh' >> ~/.profile
+
 source ~/.profile
 make install/dev-container
+
+chsh -s $(which zsh) root
