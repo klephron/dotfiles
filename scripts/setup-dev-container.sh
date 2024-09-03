f42e88baf67067348a8d1f03a29a83500fbada98
Nikita Kulakov <nvkulakov.dev@gmail.com>
2024-09-03 12:47:30 +0300

    fix path

--- scripts/setup-dev-container.sh
+++ scripts/setup-dev-container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "export XDG_CONFIG_HOME=~/.config" >> ~/.profile
-echo "export ZDOTDIR=/home/nikit/.config/zsh" >> ~/.profile
+echo 'export ZDOTDIR=$XDG_CONFIG_HOME/zsh' >> ~/.profile
 source ~/.profile
 make install/dev-container
