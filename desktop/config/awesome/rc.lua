5bba5d6948d3d51a7441a636ff29b677d708f4fb
Nikita Kulakov <nvkulakov.dev@gmail.com>
2024-09-07 14:10:27 +0300

    awesome: set default layout tiling

--- desktop/config/awesome/rc.lua
+++ desktop/config/awesome/rc.lua
@@ -89,16 +89,16 @@ local browser        = "firefox"
 awful.util.terminal  = terminal
 awful.util.tagnames  = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" }
 awful.layout.layouts = {
-  awful.layout.suit.floating,
   awful.layout.suit.tile,
   awful.layout.suit.tile.left,
   --awful.layout.suit.tile.bottom,
   --awful.layout.suit.tile.top,
-  awful.layout.suit.fair,
+  -- awful.layout.suit.fair,
   awful.layout.suit.fair.horizontal,
   --awful.layout.suit.spiral,
   --awful.layout.suit.spiral.dwindle,
   awful.layout.suit.max,
+  awful.layout.suit.floating,
   --awful.layout.suit.max.fullscreen,
   --awful.layout.suit.magnifier,
   --awful.layout.suit.corner.nw,
