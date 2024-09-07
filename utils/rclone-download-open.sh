cae5b51d45772fec2593aed56c78c5e46900b6ac
Nikita Kulakov <nvkulakov.dev@gmail.com>
2024-09-07 13:21:10 +0300

    rclone: fix when cloning with directory

--- utils/rclone-download-open.sh
+++ utils/rclone-download-open.sh
@@ -8,10 +8,10 @@ file=$1
 drive=$(realpath -s --relative-to="$drives_path" "$file" | cut -d'/' -f1)
 drive_file=$(realpath -s --relative-to="$drives_path/$drive" "$file")
 
-mkdir -pv $download_path
-
-rclone copy -P --stats 100ms "$drive:$drive_file" "$download_path"
+mkdir -pv "$download_path"
 
 file_name=$(basename "$drive_file")
+
+rclone copy -P --stats 100ms "$drive:$drive_file" "$download_path/$file_name"
 
 xdg-open "$download_path/$file_name"
