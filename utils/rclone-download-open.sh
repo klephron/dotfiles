bbe9be5468351f05b1768fe591bb775bbc1b8386
Nikita Kulakov <nvkulakov.dev@gmail.com>
2024-09-07 13:13:16 +0300

    update

---
+++ utils/rclone-download-open.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+drives_path=$HOME/Documents/clouds
+download_path=$HOME/Downloads/rclone
+
+file=$1
+
+drive=$(realpath -s --relative-to="$drives_path" "$file" | cut -d'/' -f1)
+drive_file=$(realpath -s --relative-to="$drives_path/$drive" "$file")
+
+mkdir -pv $download_path
+
+rclone copy -P --stats 100ms "$drive:$drive_file" "$download_path"
+
+file_name=$(basename "$drive_file")
+
+xdg-open "$download_path/$file_name"
