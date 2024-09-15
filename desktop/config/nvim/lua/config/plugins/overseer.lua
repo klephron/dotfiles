9009834eff8ad2e3ef4474e3770414f1cdbeab44
Nikita Kulakov <nvkulakov.dev@gmail.com>
2024-09-15 13:59:49 +0300

    nvim: open in toggleterm

--- desktop/config/nvim/lua/config/plugins/overseer.lua
+++ desktop/config/nvim/lua/config/plugins/overseer.lua
@@ -3,7 +3,12 @@ local M = {
   dependencies = {
     "mfussenegger/nvim-dap",
   },
-  opts = {},
+  opts = {
+    strategy = {
+      "toggleterm",
+      quit_on_exit = "success",
+    },
+  },
   -- keys = {
   --   { "<leader>o", "overseer" },
   --   { "<leader>o", "<cmd>Overseer<cr>", desc = "whichkey: Open" },
