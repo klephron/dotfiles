4b101a173bfffffe04b62f2d7a03611660b1a834
Nikita Kulakov <nvkulakov.dev@gmail.com>
2025-01-10 20:25:07 +0300

    nvim: disable copilot

--- desktop/config/nvim/lua/config/plugins/copilot.lua
+++ desktop/config/nvim/lua/config/plugins/copilot.lua
@@ -1,6 +1,7 @@
 local M = {
   "zbirenbaum/copilot.lua",
   event = "InsertEnter",
+  enabled = false,
   config = function()
     require("copilot").setup({
       suggestion = { enabled = false },
