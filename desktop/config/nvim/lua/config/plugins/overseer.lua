ff81a7b8a4d4c1518652ba27cc9f348ba37c8bb9
Nikita Kulakov <nvkulakov.dev@gmail.com>
2025-02-05 03:05:11 +0300

    nvim: overseer update mappings

--- desktop/config/nvim/lua/config/plugins/overseer.lua
+++ desktop/config/nvim/lua/config/plugins/overseer.lua
@@ -4,12 +4,25 @@ local M = {
     "mfussenegger/nvim-dap",
   },
   event = "VeryLazy",
-  opts = {
-    strategy = {
-      "toggleterm",
-      quit_on_exit = "success",
-    },
+  keys = {
+    { "<leader>o", group = "overseer" },
+    { "<leader>o", "<cmd>OverseerOpen<cr>",   "overseer: open" },
+    { "<leader>O", "<cmd>OverseerToggle<cr>", "overseer: toggle" },
   },
+  config = function()
+    local overseer = require("overseer")
+    overseer.setup({
+      strategy = {
+        "toggleterm",
+        quit_on_exit = "success",
+      },
+      task_list = {
+        bindings = {
+          ["q"] = "<cmd>OverseerClose<cr>"
+        }
+      }
+    })
+  end
 }
 
 return M
