4e88e66f88c4276618d15fb9b1a944dc12b4e32a
Nikita Kulakov <nvkulakov.dev@gmail.com>
2025-05-05 12:03:53 +0300

    add vimtex build dir

--- desktop/config/nvim/lua/config/plugins/vimtex.lua
+++ desktop/config/nvim/lua/config/plugins/vimtex.lua
@@ -10,6 +10,17 @@ local M = {
     vim.g.vimtex_matchparen_enabled = 0
     vim.g.vimtex_syntax_enabled = 0
     -- vim.cmd("hi clear Conceal")
+    vim.g.vimtex_compiler_latexmk = {
+      aux_dir = 'build',
+      out_dir = 'build',
+      options = {
+        '-xelatex',
+        '-verbose',
+        '-file-line-error',
+        '-synctex=1',
+        '-interaction=nonstopmode',
+      },
+    }
   end
 }
 
