488f78d192c983cd42cafb605286b9786c611e4b
Nikita Kulakov <klephron.dev@gmail.com>
2025-09-02 12:49:43 +1000

    zsh: add aliases for wayland clipboard

--- profiles/desktop/.config/zsh/functions.zsh
+++ profiles/desktop/.config/zsh/functions.zsh
@@ -17,6 +17,8 @@ function asdf-latest () {
 function clipboard-get () {
   if [[ -x "$(command -v termux-clipboard-get)" ]]; then
     termux-clipboard-get "$@"
+  elif [[ $XDG_SESSION_TYPE == "wayland" ]] && [[ -x "$(command -v wl-paste)" ]]; then
+    wl-paste "$@"
   else
     echo "error: unable to discover clipboard" 1>&2
   fi
@@ -25,6 +27,8 @@ function clipboard-get () {
 function clipboard-set () {
   if [[ -x "$(command -v termux-clipboard-set)" ]]; then
     termux-clipboard-set "$@"
+  elif [[ $XDG_SESSION_TYPE == "wayland" ]] && [[ -x "$(command -v wl-copy)" ]]; then
+    wl-copy "$@"
   else
     echo "error: unable to discover clipboard" 1>&2
   fi
