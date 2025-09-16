2d96bee42133047ca92b2ee60a6f1adc933edabf
Nikita Kulakov <klephron.dev@gmail.com>
2025-09-16 23:54:16 +1000

    nvim: fix neovide preset

--- profiles/desktop/.config/nvim/lua/profiles/neovide.lua
+++ profiles/desktop/.config/nvim/lua/profiles/neovide.lua
@@ -1,4 +1,4 @@
-vim.opt.guifont = "FiraCode Nerd Font Mono:h6.9"
+vim.opt.guifont = "FiraCode Nerd Font Mono:h12"
 vim.g.neovide_remember_window_size = true
 vim.g.neovide_profiler = false
 vim.g.neovide_input_use_logo = true
@@ -10,4 +10,4 @@ vim.g.neovide_floating_opacity = 1
 vim.g.neovide_floating_blur_amount_x = 2.0
 vim.g.neovide_floating_blur_amount_y = 2.0
 
-require("presets.default")
+require("profiles.default")
