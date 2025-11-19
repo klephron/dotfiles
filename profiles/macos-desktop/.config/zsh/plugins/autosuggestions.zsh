a8b4aba79d288f2ddbb4f94852cf4bd58d91ff53
Nikita Kulakov <klephron.dev@gmail.com>
2025-11-19 22:32:24 +0300

    rm plugin configurations

--- profiles/macos-desktop/.config/zsh/plugins/autosuggestions.zsh
+++
@@ -1,6 +0,0 @@
-source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#777777"
-ZSH_AUTOSUGGEST_STRATEGY="history"
-
-bindkey '^O' autosuggest-accept
--- profiles/macos-desktop/.config/zsh/plugins/fzf.zsh
+++
@@ -1,4 +0,0 @@
-source /usr/share/fzf/key-bindings.zsh
-if [[ -f ~/.config/fzf ]]; then
-  source ~/.config/fzf
-fi
