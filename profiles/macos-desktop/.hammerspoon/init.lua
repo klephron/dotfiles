1ed3d8ee8458249a523045a20ee381b55c883fc9
Nikita Kulakov <klephron.dev@gmail.com>
2025-11-22 12:49:02 +0300

    hammerspoon: bind cmd+return to open terminal

---
+++ profiles/macos-desktop/.hammerspoon/init.lua
@@ -0,0 +1,11 @@
+hs.hotkey.bind({ "cmd" }, "return", function()
+  hs.execute('open -na "kitty"')
+end)
+
+-- hs.hotkey.bind({ "cmd" }, "c", function()
+--   hs.execute('open -na "Firefox"')
+-- end)
+
+-- hs.hotkey.bind({ "cmd", "shift" }, "c", function()
+--   hs.execute('open -na "Google Chrome"')
+-- end)
