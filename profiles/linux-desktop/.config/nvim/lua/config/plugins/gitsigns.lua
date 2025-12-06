8271b217f0d40a95a5db8da9773e7ea8a7a8bc9a
Nikita Kulakov <klephron.dev@gmail.com>
2025-12-06 19:05:55 +0300

    add gitsigns refresh

--- profiles/linux-desktop/.config/nvim/lua/config/plugins/gitsigns.lua
+++ profiles/linux-desktop/.config/nvim/lua/config/plugins/gitsigns.lua
@@ -1,6 +1,6 @@
 local M = {
   'lewis6991/gitsigns.nvim',
-  event = "VeryLazy",
+  event = "BufRead",
   config = function()
     local gitsigns = require("gitsigns")
 
@@ -16,6 +16,7 @@ local M = {
 
   keys = {
     { "<leader>gb",      "<cmd>Gitsigns blame_line<cr>",                mode = "n", desc = "Blame line" },
+    { "<leader>gr",      "<cmd>Gitsigns refresh<cr>",                   mode = "n", desc = "Refresh" },
     { "<localleader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", mode = "n", desc = "Toggle current line blame" },
   }
 }
