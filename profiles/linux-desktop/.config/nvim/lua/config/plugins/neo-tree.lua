3bc28123ebc78fb6d172e0d028a227c595bb150c
Nikita Kulakov <klephron.dev@gmail.com>
2025-12-06 14:17:58 +0300

    refactor neo-tree

--- profiles/linux-desktop/.config/nvim/lua/config/plugins/neo-tree.lua
+++ profiles/linux-desktop/.config/nvim/lua/config/plugins/neo-tree.lua
@@ -1,3 +1,75 @@
+local C = {
+  system_open = function(state)
+    local node = state.tree:get_node()
+    local path = node:get_id()
+    -- Mac OSX
+    vim.api.nvim_command("silent !open -g " .. path)
+    -- Linux
+    vim.api.nvim_command(string.format("silent !xdg-open '%s'", path))
+  end,
+
+  focus_parent = function(state)
+    local node = state.tree:get_node()
+    require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+  end,
+
+  up = function(state)
+    local node = state.tree:get_node()
+    if not node then return end
+    require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+  end,
+
+  down = function(state)
+    local node = state.tree:get_node()
+
+    if not node or not (node.type == "directory" or node:has_children()) then
+      return
+    end
+
+    local function callback()
+      if node:has_children() then
+        require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
+      end
+    end
+
+    if node:is_expanded() then
+      callback()
+      return
+    end
+
+    state.commands.toggle_node(state)
+
+    local timer = vim.uv.new_timer()
+    local start = vim.uv.now()
+
+    timer:start(0, 10, function()
+      if node:is_expanded() then
+        timer:stop()
+        timer:close()
+        vim.schedule(callback)
+      elseif vim.uv.now() - start > 500 then
+        timer:stop()
+        timer:close()
+      end
+    end)
+  end,
+
+  open = function(state)
+    require("neo-tree.ui.renderer").focus_node(state, state.tree:get_node():get_id())
+    require("neo-tree.sources.filesystem.commands").open(state)
+  end,
+
+  open_split = function(state)
+    require("neo-tree.ui.renderer").focus_node(state, state.tree:get_node():get_id())
+    require("neo-tree.sources.filesystem.commands").open_split(state)
+  end,
+
+  open_vsplit = function(state)
+    require("neo-tree.ui.renderer").focus_node(state, state.tree:get_node():get_id())
+    require("neo-tree.sources.filesystem.commands").open_vsplit(state)
+  end,
+}
+
 local M = {
   "nvim-neo-tree/neo-tree.nvim",
   dependencies = {
@@ -19,76 +91,6 @@ local M = {
   config = function()
     local neo_tree = require("neo-tree")
     local funcs = require("utils.funcs")
-
-    local system_open = function(state)
-      local node = state.tree:get_node()
-      local path = node:get_id()
-      -- Mac OSX
-      vim.api.nvim_command("silent !open -g " .. path)
-      -- Linux
-      vim.api.nvim_command(string.format("silent !xdg-open '%s'", path))
-    end
-
-    local focus_parent = function(state)
-      local node = state.tree:get_node()
-      require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
-    end
-
-    local up = function(state)
-      local node = state.tree:get_node()
-      if not node then return end
-      require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
-    end
-
-    local down = function(state)
-      local node = state.tree:get_node()
-
-      if not node or not (node.type == "directory" or node:has_children()) then
-        return
-      end
-
-      local function callback()
-        if node:has_children() then
-          require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
-        end
-      end
-
-      if node:is_expanded() then
-        callback()
-        return
-      end
-
-      state.commands.toggle_node(state)
-
-      local timer = vim.uv.new_timer()
-      local start = vim.uv.now()
-
-      timer:start(0, 10, function()
-        if node:is_expanded() then
-          timer:stop()
-          timer:close()
-          vim.schedule(callback)
-        elseif vim.uv.now() - start > 500 then
-          timer:stop()
-          timer:close()
-        end
-      end)
-    end
-
-    local open = function(state)
-      require("neo-tree.ui.renderer").focus_node(state, state.tree:get_node():get_id())
-      require("neo-tree.sources.filesystem.commands").open(state)
-    end
-
-    local open_split = function(state)
-      require("neo-tree.ui.renderer").focus_node(state, state.tree:get_node():get_id())
-      require("neo-tree.sources.filesystem.commands").open_split(state)
-    end
-
-    local open_vsplit = function(state)
-      require("neo-tree.ui.renderer").focus_node(state, state.tree:get_node():get_id())
-      require("neo-tree.sources.filesystem.commands").open_vsplit(state)
-    end
 
     neo_tree.setup({
       filesystem = {
@@ -121,13 +123,13 @@ local M = {
           }
         },
         commands = {
-          system_open = system_open,
-          focus_parent = focus_parent,
-          up = up,
-          down = down,
-          open = open,
-          open_split = open_split,
-          open_vsplit = open_vsplit,
+          system_open = C.system_open,
+          focus_parent = C.focus_parent,
+          up = C.up,
+          down = C.down,
+          open = C.open,
+          open_split = C.open_split,
+          open_vsplit = C.open_vsplit,
         },
       }
     })
