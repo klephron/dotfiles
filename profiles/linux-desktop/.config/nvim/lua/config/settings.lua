8f4f13e17a899968c6e9158ff08f09dbe020d065
Nikita Kulakov <klephron.dev@gmail.com>
2026-01-06 23:38:26 +0300

    add smartcase

--- profiles/linux-desktop/.config/nvim/lua/config/settings.lua
+++ profiles/linux-desktop/.config/nvim/lua/config/settings.lua
@@ -145,6 +145,7 @@ vim.opt.showmatch = false
 vim.opt.incsearch = true
 vim.opt.hlsearch = true
 vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.opt.matchpairs:append("<:>")
 ----------------------------------------------------------------------------//
