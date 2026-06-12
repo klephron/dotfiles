3ce082f76f43c4d246045bad25281e4d3a5ba781
Nikita Kulakov <klephron.dev@gmail.com>
2026-06-12 23:13:39 +0300

    add window rules

--- profiles/linux-desktop/.config/hypr/hyprland/rules.lua
+++ profiles/linux-desktop/.config/hypr/hyprland/rules.lua
@@ -1,15 +1,63 @@
--- windowrulev2 = noborder, fullscreen:1
---
--- # WINDOW SHARE PICKER
--- windowrulev2 = float,class:^(hyprland-share-picker)
---
--- # ZOOM
--- # NOTE: currently add xwayland=true in $XDG_CONFIG_HOME/zoomus.conf
--- # otherwise many features will not work (whiteboard, annotations and etc.)
--- windowrulev2 = float,class:^(Zoom Workplace)$,title:^(as_toolbar)$
--- windowrulev2 = float,class:^(Zoom Workplace)$,title:(popup)$
---
--- # https://github.com/AlynxZhou/showmethekey
--- windowrulev2 = float,class:^(showmethekey-gtk)$
--- windowrulev2 = noborder,class:^(showmethekey-gtk)$
--- #windowrulev2 = pin,class:^(showmethekey-gtk)$
+-- no border when fullscreen or only one tiled window visible in workspace
+hl.window_rule({
+  match = {
+    fullscreen = true,
+  },
+  border_size = 0,
+})
+
+hl.window_rule({
+  match = {
+    workspace = "w[tv1]",
+  },
+  border_size = 0,
+})
+
+-- WINDOW SHARE PICKER
+hl.window_rule({
+  match = {
+    class = "hyprland-share-picker",
+  },
+  float = true,
+})
+
+-- ZOOM
+-- NOTE: currently add xwayland=true in $XDG_CONFIG_HOME/zoomus.conf
+-- otherwise many features will not work: whiteboard, annotations, etc.
+hl.window_rule({
+  match = {
+    class = "Zoom Workplace",
+    title = "as_toolbar",
+  },
+  float = true,
+})
+
+hl.window_rule({
+  match = {
+    class = "Zoom Workplace",
+    title = "popup",
+  },
+  float = true,
+})
+
+-- https://github.com/AlynxZhou/showmethekey
+hl.window_rule({
+  match = {
+    class = "showmethekey-gtk",
+  },
+  float = true,
+})
+
+hl.window_rule({
+  match = {
+    class = "showmethekey-gtk",
+  },
+  border_size = 0,
+})
+
+-- hl.window_rule({
+--   match = {
+--     class = "showmethekey-gtk",
+--   },
+--   pin = true,
+-- })
