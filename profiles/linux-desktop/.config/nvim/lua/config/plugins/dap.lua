595abb8860ff325f8cf6adf3b76381f94047b7e5
Nikita Kulakov <klephron.dev@gmail.com>
2026-07-07 23:14:51 +0300

    refactor simplify dap and lsp config

--- profiles/linux-desktop/.config/nvim/lua/config/plugins/dap.lua
+++ profiles/linux-desktop/.config/nvim/lua/config/plugins/dap.lua
@@ -1,10 +1,26 @@
-local M = {
+local M;
+
+local dap = require("dap")
+local widgets = require("dap.ui.widgets")
+local home = vim.fn.expand('$HOME')
+local kmps_dict = require("utils.kmps_dict").KeymapDictionary
+local utils = require("utils.funcs")
+
+local kmps_dap
+local kmps_dap_fn
+local launchjs_paths
+local is_launchjs_ok
+local ttft
+local funcs
+local dap_notify
+local process_launchjs
+local keymaps_setup
+local langs_setup
+
+M = {
   "mfussenegger/nvim-dap",
   event = "VeryLazy",
   config = function()
-    local keymaps = require("config.dap.keymaps")
-    local langs = require("config.dap.langs")
-
     vim.api.nvim_set_hl(0, 'DapBreakpoint',
       { ctermbg = 0, fg = "#e41400", bg = nil }
     )
@@ -47,9 +63,335 @@ local M = {
         numhl = 'DapStopped'
       })
 
-    keymaps.setup()
-    langs.setup()
+    keymaps_setup()
+    langs_setup()
   end
 }
 
+kmps_dap = kmps_dict:new({
+  -- Main
+  step_over = { "<leader>dn", "Step over" },
+  step_into = { "<leader>di", "Step into" },
+  step_out = { "<leader>do", "Step out" },
+  step_back = { "<leader>dd", "Step back" },
+  run_to_cursor = { "<leader>da", "Run to cursor" },
+
+  up = { "<leader>d[", "Up" },
+  down = { "<leader>d]", "Down" },
+
+  continue = { "<leader>dc", "Continue" },
+  run_last = { "<leader>dp", "Run last" },
+  terminate = { "<leader>dt", "Terminate debug session" },
+
+  set_breakpoint_cond = { "<leader>de", "Set conditional break" },
+  set_breakpoint_log = { "<leader>dm", "Set log break" },
+  toggle_breakpoint = { "<leader>ds", "Toggle breakpoint" },
+  clear_breakpoints = { "<leader>dz", "Clear breakpoints" },
+
+  hover = { "<leader>dh", "Hover" }, -- show variable
+  dapui_toggle = { "<leader>dy", "Toggle dapui" },
+
+  -- Optional
+  process_launchjs = { "<leader>dl", "Process launchjs" },
+  disconnect = { "<leader>db", "Disconnect from debuggee and terminate adapter" },
+  -- close = { "<leader>dq", "Close" },
+  -- pause = {"<leader>dp", "Pause"},
+  reverse_continue = { "<leader>dr", "Reverse continue" },
+  repl_toggle = { "<leader>dg", "Toggle REPL" },
+  -- repl_session = { "<leader>dx", "REPL session" },
+}, "")
+
+kmps_dap_fn = kmps_dict:new({
+  step_into = { "<F1>", "Step into" },
+  step_over = { "<F2>", "Step over" },
+  step_out = { "<F3>", "Step out" },
+  run_to_cursor = { "<F4>", "Run to cursor", },
+  step_back = { "<F5>", "Step back" },
+
+  hover = { "<F6>", "Open hover" },
+
+  run_last = { "<F7>", "Run last" },
+  continue = { "<F8>", "Continue" },
+
+  toggle_breakpoint = { "<F9>", "Toggle breakpoint" },
+  clear_breakpoints = { "<F33>", "Clear breakpoints" }, -- Ctrl + <F9>
+
+  terminate = { "<F10>", "Terminate" },
+}, "")
+
+
+launchjs_paths = {
+  'launch.json',
+  '.vscode/launch.json',
+}
+
+is_launchjs_ok = nil
+
+ttft = {}
+
+funcs = {
+  continue = function()
+    -- Force load dap-ui
+    require("dapui")
+    if is_launchjs_ok == nil then
+      is_launchjs_ok = process_launchjs({ ask = false })
+    end
+    return dap.continue()
+  end,
+
+  run_last = function()
+    return dap.run_last()
+  end,
+
+  process_launchjs_ask = function()
+    return process_launchjs({ ask = true })
+  end,
+
+  terminate = function()
+    return dap.terminate()
+  end,
+
+  disconnect = function()
+    return dap.disconnect()
+  end,
+
+  close = function()
+    return dap.close()
+  end,
+
+  set_breakpoint_cond = function()
+    return dap.set_breakpoint(vim.fn.input("[Condition]: "))
+  end,
+
+  set_breakpoint_log = function()
+    return dap.set_breakpoint(nil, nil, vim.fn.input('[Log message]: '))
+  end,
+
+  toggle_breakpoint = function()
+    return dap.toggle_breakpoint()
+  end,
+
+  clear_breakpoints = function()
+    return dap.clear_breakpoints()
+  end,
+
+  step_over = function()
+    return dap.step_over()
+  end,
+
+  step_into = function()
+    return dap.step_into()
+  end,
+
+  step_out = function()
+    return dap.step_out()
+  end,
+
+  step_back = function()
+    return dap.step_back()
+  end,
+
+  reverse_continue = function()
+    return dap.reverse_continue()
+  end,
+
+  up = function()
+    return dap.up()
+  end,
+
+  down = function()
+    return dap.down()
+  end,
+
+  run_to_cursor = function()
+    return dap.run_to_cursor()
+  end,
+
+  repl_toggle = function()
+    -- local height = vim.fn.winwidth(vim.fn.win_getid())
+    local h = vim.api.nvim_win_get_height(0)
+    return dap.repl.toggle({ height = math.floor(h * 0.3) }, "belowright split")
+  end,
+
+  hover = function()
+    return widgets.hover()
+  end,
+
+  dapui_toggle = function()
+    return require("dapui").toggle({ reset = true })
+  end,
+}
+
+
+dap_notify = function(msg, level)
+  vim.notify(msg, level, { title = "Dap" })
+end
+
+---Process launchjs.json
+---@param opts table|nil
+---ask nil|boolean
+---@return boolean
+process_launchjs = function(opts)
+  opts = opts or {}
+  local path = nil
+  for _, ipath in ipairs(launchjs_paths) do
+    local resolved_path = vim.fn.getcwd() .. '/' .. ipath
+    if vim.loop.fs_stat(resolved_path) then
+      path = resolved_path
+      break
+    end
+  end
+  if path == nil then
+    if opts.ask then
+      path = vim.fn.input('Path to launch.json: ', vim.fn.getcwd() .. '/', 'file')
+      if not vim.loop.fs_stat(path) then
+        dap_notify("Not resolved path: " .. path .. "\nReturning...", vim.log.levels.ERROR)
+        return false
+      end
+    else
+      return false
+    end
+  end
+
+  local ok, result = pcall(require("dap.ext.vscode").load_launchjs, path, ttft)
+  if ok then
+    dap_notify("Resolved launch.json path: " .. path, vim.log.levels.INFO)
+  else
+    dap_notify(result, vim.log.levels.ERROR)
+  end
+  return ok
+end
+
+
+keymaps_setup = function()
+  utils.set_keynomap("n", kmps_dap.continue.key, funcs.continue, kmps_dap:desc("continue"))
+  utils.set_keynomap("n", kmps_dap.run_last.key, funcs.run_last, kmps_dap:desc("run_last"))
+  utils.set_keynomap("n", kmps_dap.process_launchjs.key, funcs.process_launchjs_ask, kmps_dap:desc("process_launchjs"))
+  utils.set_keynomap("n", kmps_dap.terminate.key, funcs.terminate, kmps_dap:desc("terminate"))
+  utils.set_keynomap("n", kmps_dap.disconnect.key, funcs.disconnect, kmps_dap:desc("disconnect"))
+  -- us.set_keynomap("n", kmps_dap.close.key, funcs.close, kmps_dap:desc("close"))
+  utils.set_keynomap("n", kmps_dap.set_breakpoint_cond.key, funcs.set_breakpoint_cond,
+    kmps_dap:desc("set_breakpoint_cond"))
+  utils.set_keynomap("n", kmps_dap.set_breakpoint_log.key, funcs.set_breakpoint_log, kmps_dap:desc("set_breakpoint_log"))
+  utils.set_keynomap("n", kmps_dap.toggle_breakpoint.key, funcs.toggle_breakpoint, kmps_dap:desc("toggle_breakpoint"))
+  utils.set_keynomap("n", kmps_dap.clear_breakpoints.key, funcs.clear_breakpoints, kmps_dap:desc("clear_breakpoints"))
+  utils.set_keynomap("n", kmps_dap.step_over.key, funcs.step_over, kmps_dap:desc("step_over"))
+  utils.set_keynomap("n", kmps_dap.step_into.key, funcs.step_into, kmps_dap:desc("step_into"))
+  utils.set_keynomap("n", kmps_dap.step_out.key, funcs.step_out, kmps_dap:desc("step_out"))
+  utils.set_keynomap("n", kmps_dap.step_back.key, funcs.step_back, kmps_dap:desc("step_back"))
+  -- us.set_keynomap("n", kmps_dap.pause.key, dap.pause, kmps_dap:desc("pause"))
+  utils.set_keynomap("n", kmps_dap.reverse_continue.key, funcs.reverse_continue, kmps_dap:desc("reverse_continue"))
+  utils.set_keynomap("n", kmps_dap.up.key, funcs.up, kmps_dap:desc("up"))
+  utils.set_keynomap("n", kmps_dap.down.key, funcs.down, kmps_dap:desc("down"))
+  utils.set_keynomap("n", kmps_dap.run_to_cursor.key, funcs.run_to_cursor, kmps_dap:desc("run_to_cursor"))
+  utils.set_keynomap("n", kmps_dap.repl_toggle.key, funcs.repl_toggle, kmps_dap:desc("repl_toggle"))
+  -- us.set_keynomap("n", kmps_dap.repl_session.key, dap.repl.session, kmps_dap:desc("repl_session"))
+  utils.set_keynomap("n", kmps_dap.hover.key, funcs.hover, kmps_dap:desc("hover"))
+  utils.set_keynomap("n", kmps_dap.dapui_toggle.key, funcs.dapui_toggle, kmps_dap:desc("dapui_toggle"))
+
+  -- Bind FN mappings for dap
+  utils.set_keynomap("n", kmps_dap_fn.step_over.key, funcs.step_over, kmps_dap_fn:desc("step_over"))
+  utils.set_keynomap("n", kmps_dap_fn.step_into.key, funcs.step_into, kmps_dap_fn:desc("step_into"))
+  utils.set_keynomap("n", kmps_dap_fn.step_out.key, funcs.step_out, kmps_dap_fn:desc("step_out"))
+  utils.set_keynomap("n", kmps_dap_fn.step_back.key, funcs.step_back, kmps_dap_fn:desc("step_back"))
+  utils.set_keynomap("n", kmps_dap_fn.run_to_cursor.key, funcs.run_to_cursor, kmps_dap_fn:desc("run_to_cursor"))
+
+  utils.set_keynomap("n", kmps_dap_fn.continue.key, funcs.continue, kmps_dap_fn:desc("continue"))
+  utils.set_keynomap("n", kmps_dap_fn.run_last.key, funcs.run_last, kmps_dap_fn:desc("run_last"))
+  utils.set_keynomap("n", kmps_dap_fn.terminate.key, funcs.terminate, kmps_dap_fn:desc("terminate"))
+
+  utils.set_keynomap("n", kmps_dap_fn.toggle_breakpoint.key, funcs.toggle_breakpoint,
+    kmps_dap_fn:desc("toggle_breakpoint"))
+  utils.set_keynomap("n", kmps_dap_fn.clear_breakpoints.key, funcs.clear_breakpoints,
+    kmps_dap_fn:desc("clear_breakpoints"))
+
+  utils.set_keynomap("n", kmps_dap_fn.hover.key, funcs.hover, kmps_dap_fn:desc("hover"))
+end
+
+langs_setup = function()
+  -- CXX C RUST {{{1
+  dap.adapters.cppdbg = {
+    type = 'executable',
+    command = home .. '/.local/share/nvim/mason/bin/OpenDebugAD7',
+    id = 'cppdbg', -- used in initialized request
+    enrich_config = function(config, on_config)
+      if config.preLaunchTask then
+        vim.fn.jobstart(config.preLaunchTask, {
+          on_exit = function()
+            on_config(config)
+          end
+        })
+      else
+        on_config(config)
+      end
+    end
+  }
+
+
+  dap.configurations.cpp = {
+    {
+      name = "[base]: launch `path-to-executable`",
+      type = "cppdbg",
+      request = "launch",
+      -- opts
+      program = function()
+        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      end,
+      cwd = '${workspaceFolder}',
+      stopAtEntry = true,
+    },
+    {
+      name = '[base]: attach gdbserver :1234 `path-to-executable`',
+      type = 'cppdbg',
+      request = 'launch',
+      -- opts
+      program = function()
+        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      end,
+      cwd = '${workspaceFolder}',
+      linux = {
+        MIMode = 'gdb',
+        miDebuggerServerAddress = 'localhost:1234',
+        miDebuggerPath = '/usr/bin/gdb',
+      },
+    },
+  }
+  dap.configurations.c = dap.configurations.cpp
+  dap.configurations.rust = dap.configurations.cpp
+
+  ttft.cppdbg = { "c", "cpp", "rust" }
+  -- }}}
+
+  -- PYTHON {{{1
+  require('dap-python').setup('~/Data/venvs/debugpy/bin/python')
+
+  --[[ dap.adapters.python = {
+    type = 'executable';
+    command = HOME .. '/.venvs/debugpy/bin/python',
+    args = { '-m', 'debugpy.adapter' };
+  }
+  dap.configurations.python = {
+    {
+      type = 'python';
+      request = 'launch';
+      name = "Launch file";
+
+      program = "${file}";
+      pythonPath = function()
+        local cwd = vim.fn.getcwd()
+        if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+          return cwd .. '/venv/bin/python'
+        elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+          return cwd .. '/.venv/bin/python'
+        else
+          return '/usr/bin/python'
+        end
+      end;
+    },
+  } ]]
+  -- }}}
+end
+
 return M
+
+-- vim:foldmethod=marker
--- profiles/linux-desktop/.config/nvim/lua/config/plugins/lspconfig.lua
+++ profiles/linux-desktop/.config/nvim/lua/config/plugins/lspconfig.lua
@@ -1,5 +1,9 @@
 local M;
 
+local keymaps_on_attach;
+local servers;
+local servers_local;
+
 M = {
   'neovim/nvim-lspconfig',
   dependencies = {
@@ -21,9 +25,7 @@ M = {
 }
 
 M.on_attach = function(client, bufnr)
-  local keymaps = require("config.lsp.keymaps")
-
-  keymaps.on_attach(client, bufnr)
+  keymaps_on_attach(client, bufnr)
 end
 
 M._options_get = function()
@@ -121,13 +123,301 @@ M._servers_enable = function(servers)
 end
 
 M.servers_config = function()
-  M._servers_config(require("config.lsp.servers"))
-  M._servers_config(require("config.lsp.servers-local"))
+  M._servers_config(servers)
+  M._servers_config(servers_local)
 end
 
 M.servers_enable = function()
-  M._servers_enable(require("config.lsp.servers"))
-  -- M._servers_enable(require("config.lsp.servers-local"))
+  M._servers_enable(servers)
+  -- M._servers_enable(require(servers_local))
 end
 
+keymaps_on_attach = function(client, bufnr)
+  local funcs = require("utils.funcs")
+
+  local lspsaga_ok = false
+
+  local function wrap(desc)
+    return { buffer = bufnr, desc = desc, nowait = true }
+  end
+
+  -- definitionProvider
+  funcs.set_keynomap("n", "<leader>kd", vim.lsp.buf.definition, wrap("Goto definition"))
+  funcs.set_keynomap("n", "gd", vim.lsp.buf.definition, wrap("Goto definition"))
+
+  -- declarationProvider
+  funcs.set_keynomap("n", "<leader>kf", vim.lsp.buf.declaration, wrap("Goto declaration"))
+  funcs.set_keynomap("n", "gf", vim.lsp.buf.declaration, wrap("Goto declaration"))
+
+  -- hoverProvider
+  funcs.set_keynomap("n", "<leader>kl", function() vim.lsp.buf.hover({ border = "single", width = 80 }) end,
+    wrap("Open hover"))
+  funcs.set_keynomap("n", "gl", function() vim.lsp.buf.hover({ border = "single", width = 80 }) end,
+    wrap("Open hover"))
+
+  -- implementationProvider
+  funcs.set_keynomap("n", "<leader>ki", vim.lsp.buf.implementation, wrap("Goto implementation"))
+  funcs.set_keynomap("n", "gm", vim.lsp.buf.implementation, wrap("Goto implementation"))
+
+  -- renameProvider
+  funcs.set_keynomap("n", "<leader>kn", vim.lsp.buf.rename, wrap("Rename"))
+
+  -- referencesProvider
+  funcs.set_keynomap("n", "<leader>kr", vim.lsp.buf.references, wrap("List references"))
+  funcs.set_keynomap("n", "gr", vim.lsp.buf.references, wrap("List references"))
+
+  -- codeActionProvider
+  if lspsaga_ok then
+    funcs.set_keynomap("n", "<leader>ka", "<cmd>Lspsaga code_action<cr>", wrap("Open code action"))
+  else
+    funcs.set_keynomap("n", "<leader>ka", vim.lsp.buf.code_action, wrap("Open code action"))
+  end
+
+  -- signatureHelpProvider
+  funcs.set_keynomap("n", "<leader>kk",
+    function() vim.lsp.buf.signature_help({ border = "single", width = 80 }) end,
+    wrap("Open signature help"))
+
+  -- typeDefinitionProvider
+  funcs.set_keynomap("n", "<leader>kp", vim.lsp.buf.type_definition, wrap("Goto type definition"))
+  funcs.set_keynomap("n", "gp", vim.lsp.buf.type_definition, wrap("Goto type definition"))
+
+  -- codeLensProvider
+  funcs.set_keynomap("n", "<leader>kc", vim.lsp.codelens.run, wrap("Run codelens"))
+
+  -- documentSymbolProvider
+
+  -- documentFormattingProvider
+  --[[ -- handled by conform-nvim
+  funcs.set_keynomap("n", kmps.format.key, function() vim.lsp.buf.format({ async = true }) end,
+  with_desc(("format"))) ]]
+
+  -- selectionRangeProvider
+  funcs.set_keynomap('x', 'an', function() vim.lsp.buf.selection_range('outer') end,
+    wrap("vim.lsp.buf.selection_range('outer')"))
+  funcs.set_keynomap('x', 'in', function() vim.lsp.buf.selection_range('inner') end,
+    wrap("vim.lsp.buf.selection_range('inner')"))
+
+  -- Diagnostics
+  funcs.set_keynomap("n", "<leader>kh", vim.diagnostic.open_float, wrap("Open float"))
+  funcs.set_keynomap("n", "gh", vim.diagnostic.open_float, wrap("Open float"))
+  funcs.set_keynomap("n", "[d", vim.diagnostic.goto_prev, wrap("Goto previous diagnostics"))
+  funcs.set_keynomap("n", "]d", vim.diagnostic.goto_next, wrap("Goto next diagnostics"))
+
+  -- inlayHint
+  funcs.set_keynomap("n", "<localleader>kh",
+    function()
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+      vim.notify("inlay hints " ..
+        (vim.lsp.inlay_hint.is_enabled() and "enabled" or "disabled"), vim.log.levels.INFO, { title = "lsp" })
+    end,
+    "Toggle inlay hints")
+end
+
+servers = {
+  lua_ls = {
+    cmd = { "lua-language-server" },
+    filetypes = { "lua" },
+    single_file_support = true,
+    settings = {
+      Lua = {
+        workspace = {
+          checkThirdParty = false,
+        },
+        format = {
+          enable = true,
+        },
+        hint = {
+          enable = true,
+        },
+        semantic = {
+          enable = false,
+          variable = false,
+        }
+      },
+    },
+  },
+  clangd = {
+    cmd = { "clangd" },
+    filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+    clangd = {
+      InlayHints = {
+        Designators = true,
+        Enabled = true,
+        ParameterNames = true,
+        DeducedTypes = true,
+      },
+    },
+    capabilities = {
+      offsetEncoding = { "utf-16", "utf-8" },
+    }
+  },
+  pyright = {
+    cmd = { "pyright-langserver", "--stdio" },
+    filetypes = { "python" },
+    single_file_support = true,
+  },
+  rust_analyzer = {
+    cmd = { "rust-analyzer" },
+    filetypes = { "rust" },
+    settings = {
+      ["rust-analyzer"] = {
+        -- cachePriming = {
+        --   enable = false
+        -- },
+        -- assist = {
+        --   importEnforceGranularity = true,
+        --   importPrefix = 'crate',
+        -- },
+        -- cargo = {
+        --   features = "all",
+        --   sysrootQueryMetadata = true,
+        -- },
+        checkOnSave = {
+          command = 'check', -- used clippy, but it is really slow
+        },
+        -- inlayHints = {
+        --   bindingModeHints = {
+        --     enable = false,
+        --   },
+        --   chainingHints = {
+        --     enable = true,
+        --   },
+        --   closingBraceHints = {
+        --     enable = true,
+        --     minLines = 25,
+        --   },
+        --   closureReturnTypeHints = {
+        --     enable = "never",
+        --   },
+        --   lifetimeElisionHints = {
+        --     enable = "never",
+        --     useParameterNames = false,
+        --   },
+        --   maxLength = 25,
+        --   parameterHints = {
+        --     enable = true,
+        --   },
+        --   reborrowHints = {
+        --     enable = "never",
+        --   },
+        --   renderColons = true,
+        --   typeHints = {
+        --     enable = true,
+        --     hideClosureInitialization = false,
+        --     hideNamedConstructor = false,
+        --   },
+        --   locationLinks = false,
+        -- },
+        diagnostics = {
+          enable = true,
+          -- experimental = {
+          --   enable = true,
+          -- },
+          disabled = {
+            -- "macro-error",
+            -- "unresolved-macro-call"
+          }
+        },
+      }
+    }
+  },
+  hls = {
+    cmd = { "haskell-language-server-wrapper", "--lsp" },
+    filetypes = { 'haskell', 'lhaskell', 'cabal' },
+  },
+  yamlls = {
+    cmd = { "yaml-language-server", "--stdio" },
+    filetypes = { "yaml", "yaml.docker-compose", "yaml.gitlab" },
+    settings = {
+      yaml = {
+        schemas = {
+          ["https://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
+          ["https://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+        },
+      },
+    },
+  },
+  terraformls = {
+    cmd = { "terraform-ls", "serve" },
+    filetypes = { "terraform", "terraform-vars" },
+  },
+  jdtls = {
+    cmd = {
+      'java',
+      '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+      '-Dosgi.bundles.defaultStartLevel=4',
+      '-Declipse.product=org.eclipse.jdt.ls.core.product',
+      '-Dlog.level=ALL',
+      '-noverify',
+      '-Xmx1G',
+      "-javaagent:" .. vim.fn.stdpath("data") .. "/mason/packages/jdtls/lombok.jar",
+      '--add-modules=ALL-SYSTEM',
+      '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+      '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+      '-jar', vim.fn.glob(vim.fn.stdpath("data") .. "/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+      '-configuration', vim.fn.stdpath("data") .. "/mason/packages/jdtls/config_linux/",
+      '-data', vim.fn.stdpath("cache") .. '/jdtls/workspace/' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t'),
+    },
+    filetypes = { "java" },
+    -- settings = {
+    --   java = {
+    --     format = {
+    --       settings = {
+    --         url = vim.fn.getcwd() .. "/eclipse-java-style.xml",
+    --       }
+    --     }
+    --   }
+    -- },
+    root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', 'mvnw', '.git', '.gitignore' }, { upward = true })[1]),
+    on_init = function(client, _)
+      -- client.server_capabilities.semanticTokensProvider = nil
+    end,
+    on_attach = function()
+      local jdtls_ok, jdtls = pcall(require, "jdtls")
+      if jdtls_ok then
+        jdtls.setup_dap({ hotcodereplace = 'auto' })
+      end
+    end,
+    init_options = {
+      bundles = {
+        vim.fn.glob(
+          vim.fn.stdpath("data") ..
+          "/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar", 1)
+      }
+    }
+  },
+  lemminx = {
+    cmd = { "lemminx" },
+    filetypes = { "xml", "xsd", "xsl", "xslt", "svg" },
+  },
+  sqruff = {
+    cmd = { "sqruff" },
+    filetypes = { "sql" },
+  },
+  buf = {
+    cmd = { "buf", "beta", "lsp", "--timeout=0", "--log-format=text" },
+    filetypes = { "proto" },
+  },
+  tombi = {
+    cmd = { "tombi", "lsp" },
+    filetypes = { "toml" }
+  },
+}
+
+servers_local = {
+  -- don't forget to vim.lsp.enable("mals") when needed
+  mals = {
+    root_dir = vim.fn.getcwd(),
+    cmd = {
+      "/home/klephron/.local/share/mise/installs/go/1.25.4/bin/mals-adapter",
+      -- "-l",
+      -- vim.fn.getcwd() .. "/mals-adapter.log",
+      "-t",
+      "localhost:9651",
+      "-g",
+      "3",
+    },
+  }
+}
 return M
