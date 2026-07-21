bd8e8eef70595939f73fb9ec0633beb64f6d4712
Nikita Kulakov <klephron.dev@gmail.com>
2026-07-21 09:44:15 +0300

    disable indent options

--- profiles/linux-desktop/.config/nvim/lua/config/settings.lua
+++ profiles/linux-desktop/.config/nvim/lua/config/settings.lua
@@ -151,8 +151,9 @@ vim.opt.matchpairs:append("<:>")
 ----------------------------------------------------------------------------//
 -- Indentation
 -----------------------------------------------------------------------------//
-vim.opt.autoindent = true
-vim.opt.smartindent = true
+vim.opt.autoindent = false
+vim.opt.smartindent = false
+vim.opt.cindent = false
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
